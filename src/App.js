470f331553a55549c598e978040299a531210b5f
Clemence Guelque <penin.clemence@gmail.com>
2023-01-09 11:13:12 +0100

    creation page d'accueil

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import logo from './logo.svg';
+import { useNavigate } from 'react-router-dom';
 import './styles/App.scss';
+import Header from './components/Header'
 
-function App() {
+const App=()=>{
+  const navigate = useNavigate();
+
   return (
     <div className="App">
+      <Header/>
+      <div className="menu-App">
+        <button onClick={() => navigate("/nouveau-bon")}>Nouveau bon d'intervention</button>
+        <button onClick={() => navigate("/liste-interventions")}>Liste des interventions</button>
+      </div>
     </div>
   );
 }
--- src/Root.js
+++ src/Root.js
@@ -2,15 +2,17 @@ import React from 'react'
 import App from './App'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 // import NotFound from './components/NotFound'
+// import New from './components/New'
 // import InterventionsList from './components/InterventionsList'
 
 const Root = () => {
   return (
     <Router>
         <Routes>
-            <Route exact path="/" component={App} />
-            {/* <Route path="/liste-interventions" component={InterventionsList} /> */}
-            {/* <Route component={NotFound} /> */}
+            <Route exact path="/" element={<App />} />
+            {/* <Route path="/nouveau-bon" element={<New/>} /> */}
+            {/* <Route path="/liste-interventions" element={<InterventionsList/>} /> */}
+            {/* <Route element={<NotFound/>} /> */}
         </Routes>
     </Router>
   )
